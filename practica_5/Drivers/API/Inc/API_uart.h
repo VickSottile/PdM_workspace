c82342abc00b96d841f034b11cecafa9bebea2d1
Victoria <vicksottile@gmail.com>
2026-04-02 22:26:07 -0300

    Primera parte punto 1 - Practico 5

---
+++ practica_5/Drivers/API/Inc/API_uart.h
@@ -0,0 +1,24 @@
+/*
+ * API_uart.h
+ *
+ *  Created on: 2 abr 2026
+ *      Author: vicks
+ */
+
+#ifndef API_INC_API_UART_H_
+#define API_INC_API_UART_H_
+
+#include "main.h"
+#include "stm32f4xx_hal.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include "string.h"
+
+typedef bool bool_t;
+bool_t uartInit(); //inicializa la UART
+void uartSendString(uint8_t * pstring); //envia el  String
+void uartSendStringSize(uint8_t * pstring, uint16_t size); //envía el tamaño del string
+void uartReceiveStringSize(uint8_t * pstring, uint16_t size); // Recibe el tamaño del string
+
+
+#endif /* API_INC_API_UART_H_ */
---
+++ practica_5/Drivers/API/Src/API_uart.c
@@ -0,0 +1,60 @@
+/*
+ * API_uart.c
+ *
+ *  Created on: 2 abr 2026
+ *      Author: vicks
+ */
+#include "API_uart.h"
+
+
+UART_HandleTypeDef huart2;
+
+HAL_StatusTypeDef transmitido;
+bool_t uartInit(){ //inicializa la UART
+	  huart2.Instance = USART2;
+	  huart2.Init.BaudRate = 115200;
+	  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+	  huart2.Init.StopBits = UART_STOPBITS_1;
+	  huart2.Init.Parity = UART_PARITY_NONE;
+	  huart2.Init.Mode = UART_MODE_TX_RX;
+	  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+	  if (HAL_UART_Init(&huart2) != HAL_OK)
+	  {
+		  Error_Handler();
+		  return false;
+
+	  } else {
+		  return true;
+		  //Implementar envío de configuración
+	  }
+
+}
+void uartSendString(uint8_t *pstring) //envia el  String
+{
+	if (pstring!=NULL)
+	{
+		transmitido = HAL_UART_Transmit(&huart2, pstring, strlen((char*)pstring),100);
+		if (transmitido != HAL_OK)
+			{
+				Error_Handler();
+			}
+	}
+		//Se podria implementar un parpadeo de leds con codigo de error
+		return;
+}
+void uartSendStringSize(uint8_t * pstring, uint16_t size) {//envía el tamaño del string
+	//size entre 1 y 256 (valor razonable que se puede ajustar)
+	if ((size>=1 && size<=256)&&(pstring!=NULL))
+	{
+	transmitido = HAL_UART_Transmit(&huart2, pstring,size,100);
+	if (transmitido != HAL_OK)
+	{
+		Error_Handler();
+	}
+	}
+	//Se podria implementar un parpadeo de leds con codigo de error
+	return;
+}
+void uartReceiveStringSize(uint8_t * pstring, uint16_t size) // Recibe el tamaño del string
+{}
